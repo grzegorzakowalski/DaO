507dece66e449d76c773b68c12c43f75c8e5428f
grzegorzakowalski <grzegorz.a.kowalski@gmail.com>
2023-05-21 21:19:26 +0200

    Done

--- src/main/java/pl/coderslab/Main.java
+++ src/main/java/pl/coderslab/Main.java
@@ -1,11 +1,24 @@
 package pl.coderslab;
 
 
+import org.mindrot.jbcrypt.BCrypt;
+import pl.coderslab.entity.User;
+import pl.coderslab.entity.UserDao;
+
+import java.util.Scanner;
+
 public class Main {
 
 
     public static void main(String[] args) {
-        System.out.println("Działa");
+        UserDao userDao = new UserDao();
+        User user = new User("jan","jan.nowak@gmail.com","hasło");
+        User[] users = userDao.findAll();
+        for (User user1 : users) {
+            System.out.println(user1);
+        }
+
+
 
 
     }
